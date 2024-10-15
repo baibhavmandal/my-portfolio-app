7f1bfb7c701cdf2ac1a1998e9d9fe46f3f2bc209
baibhavmandal <baibhavmandal1@gmail.com>
2024-10-15 16:48:19 +0530

    check for errors in netlify deployement

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { aboutSection, inner, skillList } from "./about.module.css";
+import { aboutSection, inner, skillsList } from "./about.module.css";
 import { numberedHeading } from "./layout.module.css";
 
 const About = () => {
@@ -9,7 +9,7 @@ const About = () => {
       <h2 className={numberedHeading}>About Me</h2>
       <div className={inner}>
         <div>Paragraph</div>
-        <ul className={skillList}></ul>
+        <ul className={skillsList}></ul>
       </div>
     </section>
   );
